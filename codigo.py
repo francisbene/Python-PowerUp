e148784f110e6b609f603af7fd0c4ba7b2bf93d0
Francisbene Mayer <fmmcba1@gmail.com>
2024-01-09 17:24:50 -0300

    Instalando e importando pyautogui

---
+++ codigo.py
@@ -0,0 +1,25 @@
+#Passo a passo
+
+#Passo 01 - Entrar no sistema da empresa
+  ## https://dlp.hashtagtreinamentos.com/python/intensivao/login
+
+    #Instalar pyautogui - pip install pyautogui
+import pyautogui
+
+# Clicar - pyautogui.click
+# Escrever - pyautogui.write
+# apertar uma tecla - payautogui.press
+
+#Passo 02 - Fazer login
+
+#Passo 03 - Importar a base de dados
+
+#Passo 04 - Cadastrar um produto
+
+#Passo 05 - Repetir passo 04 até finalizar base de dados 
+
+
+
+
+
+
